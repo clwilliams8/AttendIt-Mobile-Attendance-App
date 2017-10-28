477540fe50eeec9716245a237b9fb6d84fcf2ec8
Colton Williams <clwilliams8@ualr.edu>
2017-10-28 17:19:54 -0500

    Instructor Navigation layout complete

--- app/src/main/java/com/example/colto/attenditdraft3/InstructorActivity.java
+++ app/src/main/java/com/example/colto/attenditdraft3/InstructorActivity.java
@@ -1,35 +1,92 @@
 package com.example.colto.attenditdraft3;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class InstructorActivity extends AppCompatActivity {
+public class InstructorActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-   // private DrawerLayout myDrawer;
-    //private ActionBarDrawerToggle myToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructor);
 
-        //myDrawer = findViewById(R.id.myDrawer);
-       //myToggle = new ActionBarDrawerToggle(this, myDrawer, R.string.open, R.string.close);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        //myDrawer.addDrawerListener(myToggle);
-        //myToggle.syncState();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.myDrawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    /*@Override
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.myDrawer);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(myToggle.onOptionsItemSelected(item)){
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
-    }*/
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.MyClasses) {
+            // Handle the camera action
+        } else if (id == R.id.CreateAClass) {
+
+        } else if (id == R.id.myRecords) {
+
+        } else if (id == R.id.logOut) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.myDrawer);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
+
