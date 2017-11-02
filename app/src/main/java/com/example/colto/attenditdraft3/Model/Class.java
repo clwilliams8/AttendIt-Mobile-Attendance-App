353d2416e7a9428fd6512a52e06dd1222e8dab40
Colton Williams <clwilliams8@ualr.edu>
2017-11-01 21:16:28 -0500

    Added Class model

---
+++ app/src/main/java/com/example/colto/attenditdraft3/Model/Class.java
@@ -0,0 +1,106 @@
+package com.example.colto.attenditdraft3.Model;
+
+/**
+ * Created by colto on 11/1/2017.
+ */
+
+public class Class {
+    private String teacherUsername;
+    private String date;
+    private String startTime;
+    private String endTime;
+    private String lateTime;
+    private String absentTime;
+    private String daysOfTheWeek;
+    private String location;
+    private String studentsEnrolled;
+
+    public Class(){
+
+    }
+
+    public Class(String teacherUsername, String date, String startTime , String endTime, String lateTime, String absentTime,
+                 String daysOfTheWeek, String location, String studentsEnrolled) {
+        this.teacherUsername = teacherUsername;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.lateTime = lateTime;
+        this.absentTime = absentTime;
+        this.daysOfTheWeek = daysOfTheWeek;
+        this.location = location;
+        this.studentsEnrolled = studentsEnrolled;
+    }
+
+    public String getTeacherUsername() {
+        return teacherUsername;
+    }
+
+    public void setTeacherUsername(String teacherUsername) {
+        this.teacherUsername = teacherUsername;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getLateTime() {
+        return lateTime;
+    }
+
+    public void setLateTime(String lateTime) {
+        this.lateTime = lateTime;
+    }
+
+    public String getAbsentTime() {
+        return absentTime;
+    }
+
+    public void setAbsentTime(String absentTime) {
+        this.absentTime = absentTime;
+    }
+
+    public String getDaysOfTheWeek() {
+        return daysOfTheWeek;
+    }
+
+    public void setDaysOfTheWeek(String daysOfTheWeek) {
+        this.daysOfTheWeek = daysOfTheWeek;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStudentsEnrolled() {
+        return studentsEnrolled;
+    }
+
+    public void setStudentsEnrolled(String studentsEnrolled) {
+        this.studentsEnrolled = studentsEnrolled;
+    }
+}
